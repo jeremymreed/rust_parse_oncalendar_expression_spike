d0fec7ef9fb5dbc1e06884c3292c40ffe9de5e43
Jeremy Reed <reeje76@gmail.com>
2024-03-03 22:13:30 -0500

    implement get_next_event(), and do parsing.
    Updated main to call systemd_analyze::get_next_event(), added code to
    parse the output from systemd-analyze, and transform it into a
    DateTime<Local>.  This DateTime<Local> gets passed back to main.
    
    This returned DateTime<Local> is the 'target' time.
    In main we have an infinite loop, that gets the current time, and checks
    it against the target time to see if the current time is the same or
    greater than the current time.
    
    It does this by getting a TimeDelta by subtracting dt_now from target.
    If this TimeDelta's num_seconds() method returns zero or a negative
    number, we fire the event.  In the case of this spike, we return, and
    exit the program immediately.
    
    This code will be used in the worker thread of walls, for the slideshow
    mode.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::{thread, time::{self, Instant}};
+use chrono::Local;
+
+mod oncalendar_parser;
+mod systemd_analyze;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Getting next event datetime!");
+
+    let sleep_duration = time::Duration::from_secs(1);
+
+    let input: String = "*-*-* *:0/2".to_string();
+    println!("input: {:?}", input);
+
+    let target  = systemd_analyze::get_next_event(&input);
+
+    loop {
+        let now = Instant::now();
+        let dt_now = Local::now();
+        println!("target: {:?}", target);
+        println!("dt_now: {:?}", dt_now);
+
+        let delta = target - dt_now;
+
+        println!("delta:        {:?}", delta);
+
+        match delta.num_seconds() <= 0 {
+            true => {
+                println!("dt_now <= target");
+                return;
+            },
+            false => println!("dt_now != target"),
+        }
+
+        let elapsed = now.elapsed();
+        println!("Elapsed time: {:#?}", elapsed);
+        println!("\n");
+
+        thread::sleep(sleep_duration);
+    }
 }
